708a6ae66b3702a790fd20cc05e5f8ddf83e09be
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-09-30 14:04:02 -0600

    Create GeneralizedHarmonic.py

---
+++ GeneralizedHarmonic.py
@@ -0,0 +1,19 @@
+import sys
+# import stdio 
+
+n = int(sys.argv[1])
+r = int(sys.argv[2])
+
+if n > 0:
+  sum = 0.0
+
+  for i in range(n): df 
+    term = 1.0 / pow(i + 1, r)
+    sum = term + sum
+
+  print(sum)
+  # stdio.writeln(sum)
+
+else:
+  print('n must to be positive')
+  # stdio.writeln('n must to be positive')
