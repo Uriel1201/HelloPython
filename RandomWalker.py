440153d990715503bea446a223273369a21195cc
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-09-30 14:43:49 -0600

    Update RandomWalker.py

--- RandomWalker.py
+++ RandomWalker.py
@@ -12,17 +12,23 @@ if r >= 0:
   steps = 0
   while manhattan < r:
     p = random()
+    
     if p < 0.25:   i = i + 1
     elif p < 0.5:  i = i - 1
     elif p < 0.75: j = j + 1
     else:          j = j - 1
+      
     print('(' + i + ',' + j + ')')
+    # stdio.writeln()
+    
     ai = abs(i)
     aj = abs(j)
     manhattan = ai + aj
     steps = steps + 1
+    
   print('Steps: ' + steps)
   # stdio.writeln()
+
 else:
   print('r must to be no negative')
   # stdio.writeln()
