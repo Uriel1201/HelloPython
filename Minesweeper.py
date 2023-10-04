0c89611ca9bb953bd49f969c9fdf354610619244
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-10-04 17:11:31 -0600

    Create Minesweeper.py

---
+++ Minesweeper.py
@@ -0,0 +1,28 @@
+# import stdio 
+import sys
+import random 
+import numpy as np 
+
+m = int()
+n = int()
+k = int()
+t = m * n
+
+shuffle = np.zeros()
+for h in range(k):
+  shuffle[h] = True
+
+for h in range():
+  p =
+  s =
+  shuffle[] = shuffle[]
+  shuffle[] = s
+
+mines = np.zeros()
+for h in 
+
+
+
+
+
+
