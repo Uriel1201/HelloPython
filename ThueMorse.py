af27b931c94302fd6fcff8ad335b16e6a137e902
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-10-04 22:17:28 -0600

    Create ThueMorse.py

---
+++ ThueMorse.py
@@ -0,0 +1,28 @@
+# import stdio
+import numpy as np 
+import sys
+
+n = int(sys.argv[1])
+
+if n >= 0:
+  a = np.zeros(n, dtype = int)
+  
+  for i in range(1, n):
+    if i % 2 == 0: 
+      a[i] = a[i / 2]
+    else: 
+      a[i] = 1 - a[i - 1]
+  for i in  range(n):
+    for j in range(n):
+      if a[i] == a[j]: 
+        print('+', end = '  ')
+        # stdio.write()
+      else: 
+        print('-', end = '  ')
+        # stdio.write()
+    print()
+    # stdio.writeln()
+  
+else: 
+  print('n must to be no negative')
+  # stdio.writeln()
