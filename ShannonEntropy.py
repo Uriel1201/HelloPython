4356fd673dfb6a52972c1cc709449b5ad1472100
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-10-20 17:02:43 -0600

    Update ShannonEntropy.py

--- ShannonEntropy.py
+++ ShannonEntropy.py
@@ -32,3 +32,24 @@ def readInt():
   s = _readingE(r'[-+]?(0[xX][\dA-Fa-f]+|0[0-7]*|\d+)')
   radix = 10
   return int(s, radix)
+
+#---------------------------------------------
+m = int(sys.argv[1]) 
+f = np.zeros(m + 1, dtype = int) 
+n = 0
+
+while not isEmpty():
+  x = readInt()
+  if x < m + 1 and x > 0:
+    f[x] = f[x] + 1
+    n = n + 1
+
+h = 0.0
+for i in range(1, m + 1):s 
+  if f[i] > 0:
+    p = 1.0 * f[i] / n 
+    arg = -p * np.log(p) / np.log(2)
+    h = h + arg
+
+print('ShannonEntropy: ', end = '') 
+print(h)
