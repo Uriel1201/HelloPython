bcc020986af9f7ae6d42627b1c1b058d3da8a77d
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-10-21 15:19:03 -0600

    Update LongestRun.py

--- LongestRun.py
+++ LongestRun.py
@@ -1,3 +1,37 @@
+import sys 
+import re 
+
+sys.stdin = open(sys.stdin.fileno(), 'r', newline = None)
+_contenedor = ''
+
+#---------------------------------------------
+def isEmpty():
+  global _contenedor 
+  while _contenedor.strip() == '':
+    line = sys.stdin.readline()
+    if line == '':
+      return True
+    _contenedor += line
+ 
+  return False 
+
+#---------------------------------------------
+def _readingE(regExp):
+  global _contenedor 
+  pattern = re.compile(r'^\s*' + regExp) 
+  match = pattern.search(_contenedor)
+  if match is None:
+    raise ValueError() 
+  _subS = match.group() 
+  _contenedor = _contenedor[match.end():] 
+  return _subS.lstrip()
+
+#---------------------------------------------
+def readInt():
+  s = _readingE(r'[-+]?(0[xX][\dA-Fa-f]+|0[0-7]*|\d+)')
+  return int(s, 10)
+
+#---------------------------------------------
 max = 0
 rep = 1
 x = readInt()
