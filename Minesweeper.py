16d37516253357087b6f619ab29dda45b98810d3
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-01-25 19:55:04 -0500

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,31 +1,35 @@
-# import stdio 
+#import stdio
 import sys
-import random 
-import numpy as np 
+import numpy as np
 
-m = int(input("Number of files?"))
-n = int(input("Number of Columns?"))
-k = int(input("How Many Mines?"))
+m = input("Number of files?") 
+n = input("Number of Columns?") 
+k = input("How Many Mines?") 
 t = m * n
 
-shuffle = np.zeros(t, dtype = bool)
+shuffle = np.zeros(t, dtype = bool) 
 shuffle[:k] = True
 
 np.random.shuffle(shuffle)
 
-mines = shuffle.reshape((m + 2, n + 2))[1: -1]
+mines = np.zeros([m + 2, n + 2], dtype = bool) 
+for h in range(1, m + 1):
+  for i in range(1, n + 1):
+    mines[h, i] = shuffle[n * (h - 1) + i - 1]
 
 count = np.zeros([m + 2, n + 2], dtype = int) 
 
 for i in range(1, m + 1):
   for j in range(1, n + 1):
-    count[i, j] = np.sum(mines[i - 1 : i + 2, j - 1 : j + 2])
-
+    count[i, j] = np.sum(mines[i - 1:i + 2, j - 1:j + 2])
+      
 for i in range(1, m + 1):
   for j in range(1, n + 1):
-    if mines[i, j]: print('*', end = '  ')
+    if mines[i, j]: 
+      print('*', end = '  ') 
       # stdio.write()
-    else: print(count[i, j], end = '  ')
-      # stdio.write()
+    else:
+      print(count[i, j], end = '  ')
+      # stdio.write() 
   print()
   # stdio.writeln()
