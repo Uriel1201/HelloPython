c09b03caf559b58127f062f2d2900548b748f268
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-02-01 02:09:35 -0500

    Update Divisors.py

--- Divisors.py
+++ Divisors.py
@@ -1,32 +1,19 @@
-# %%
-# !! {"metadata":{
-# !!   "id": "rGnRPEkCH4Y2"
-# !! }}
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# %%
-# !! {"metadata":{
-# !!   "id": "zgAiNcaZP3cQ"
-# !! }}
+
+#-----------------------------------------------------------------------
 """
 # **Divisors Methods**
 """
 
-# %%
-# !! {"metadata":{
-# !!   "id": "mgoPBDE3NrMZ"
-# !! }}
+
+#-----------------------------------------------------------------------
 """
 ### gcd(a, b):
 The greatest common divisor (gcd) of two integers a and b. By convention, gcd(0,0)=0.
 """
-
-# %%
-# !! {"metadata":{
-# !!   "id": "Q54xz9mNIFf8"
-# !! }}
 def gcd(a, b):
     if b < 0:
         b = -b
@@ -45,57 +32,35 @@ def gcd(a, b):
             b = t % a
         return a
 
-# %%
-# !! {"metadata":{
-# !!   "id": "Sq3MnNsFQOmB"
-# !! }}
-"""
 
+#-----------------------------------------------------------------------
+"""
 ### lcm(a, b):
 The least common multiple (lcm) of two integers a and b. By convention, if either a or b is 0, then lcm(a,b)=0.
 """
-
-# %%
-# !! {"metadata":{
-# !!   "id": "AD78AI1ZNp_z"
-# !! }}
 def lcm(a, b):
     if a == 0 or b == 0:
         return 0
     else:
         return np.abs(a) // gcd(a,b) * np.abs(b)
 
-# %%
-# !! {"metadata":{
-# !!   "id": "h8Jz0ij5RLFI"
-# !! }}
+
+#-----------------------------------------------------------------------
 """
 ### areRelativelyPrime(a, b):
 Are two integers a and b relatively prime?  
 Two integers are relatively prime if they share no common positive factors (divisors) except 1.
 """
-
-# %%
-# !! {"metadata":{
-# !!   "id": "tU45RoVXPmJv"
-# !! }}
 def areRelativelyPrime(a, b):
     g = gcd(a,b)
     return g == 1
 
-# %%
-# !! {"metadata":{
-# !!   "id": "0SggJFGtYCsk"
-# !! }}
+
+#-----------------------------------------------------------------------
 """
 ### dynamicPhi(n):
 This method dynamically updates the Euler’s totient function ϕ(i) for each i less than n. This approach is based on the Euler's product representation, where the product is taken over the distinct prime numbers that divide n.
 """
-
-# %%
-# !! {"metadata":{
-# !!   "id": "lpdUYImCQMER"
-# !! }}
 def dynamicPhi(n):
     if n < 0:
         raise ValueError('n must be positive')
@@ -108,18 +73,11 @@ def dynamicPhi(n):
                 phi[j] -= phi[j] // i
     return phi
 
-# %%
-# !! {"metadata":{
-# !!   "id": "xbEsgr9KbzQB"
-# !! }}
+
+#-----------------------------------------------------------------------
 """
 Finally composing a test client
 """
-
-# %%
-# !! {"metadata":{
-# !!   "id": "ZQFYYpq5ZLnK"
-# !! }}
 def main():
     x = int(input("First Integer: "))
     y = int(input("Second Integer: "))
@@ -141,39 +99,7 @@ def main():
     plt.tight_layout()
     plt.show()
 
-# %%
-# !! {"metadata":{
-# !!   "colab": {
-# !!     "base_uri": "https://localhost:8080/",
-# !!     "height": 398
-# !!   },
-# !!   "id": "gvt_mSVUcVrM",
-# !!   "executionInfo": {
-# !!     "status": "ok",
-# !!     "timestamp": 1706740489760,
-# !!     "user_tz": 360,
-# !!     "elapsed": 31200,
-# !!     "user": {
-# !!       "displayName": "Uriel Garc\u00eda",
-# !!       "userId": "03386744220426758265"
-# !!     }
-# !!   },
-# !!   "outputId": "8c13338e-c980-478d-b728-2e3f97b1fb27"
-# !! }}
+
+#-----------------------------------------------------------------------
 if __name__ == '__main__':
     main()
-
-# %%
-# !! {"main_metadata":{
-# !!   "colab": {
-# !!     "provenance": [],
-# !!     "authorship_tag": "ABX9TyN7Ao7f1dlnrGTReOR11D7v"
-# !!   },
-# !!   "kernelspec": {
-# !!     "name": "python3",
-# !!     "display_name": "Python 3"
-# !!   },
-# !!   "language_info": {
-# !!     "name": "python"
-# !!   }
-# !! }}
