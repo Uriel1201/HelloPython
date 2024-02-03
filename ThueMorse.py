506bd2d9d4661c29585bd9a56a93801b5e66a63c
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-02-03 14:45:09 -0500

    Update ThueMorse.py

--- ThueMorse.py
+++ ThueMorse.py
@@ -1,53 +1,23 @@
-# %%
-# !! {"metadata":{
-# !!   "id": "cBNq13M_DxmQ"
-# !! }}
+#----------------------------------------------------------
 """
 # Thue-Morse Sequence
-The Thue-Morse Sequence is an infinite sequence of 0s and 1s that is constructed by starting with 0 and successively appending the bitwise negation of the existing sequence.
+The Thue-Morse Sequence is an infinite sequence of 0s and 1s 
+that is constructed by starting with 0 and successively appending 
+the bitwise negation of the existing sequence.
 """
-
-# %%
-# !! {"metadata":{
-# !!   "id": "igbifJh_Dx9e",
-# !!   "executionInfo": {
-# !!     "status": "ok",
-# !!     "timestamp": 1706988851158,
-# !!     "user_tz": 360,
-# !!     "elapsed": 352,
-# !!     "user": {
-# !!       "displayName": "Uriel Garc\u00eda",
-# !!       "userId": "03386744220426758265"
-# !!     }
-# !!   }
-# !! }}
+#----------------------------------------------------------
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as pch
 from matplotlib.collections import PatchCollection
 
-# %%
-# !! {"metadata":{
-# !!   "id": "cGLpfeACb45V"
-# !! }}
+
+#----------------------------------------------------------
 """
-For visualizing this sequence, create an n-by-n pattern printing black for 0 and white for 1.
+For visualizing this sequence, create an n-by-n pattern 
+printing black for 0 and white for 1.
 """
 
-# %%
-# !! {"metadata":{
-# !!   "id": "OhkWHDY1D0d8",
-# !!   "executionInfo": {
-# !!     "status": "ok",
-# !!     "timestamp": 1706988855351,
-# !!     "user_tz": 360,
-# !!     "elapsed": 287,
-# !!     "user": {
-# !!       "displayName": "Uriel Garc\u00eda",
-# !!       "userId": "03386744220426758265"
-# !!     }
-# !!   }
-# !! }}
 def main():
     n = int(input("n-by-n grid: "))
     if n < 0:
@@ -75,39 +45,7 @@ def main():
     plt.title(f'Thue-Morse Sequence Plot')
     plt.show()
 
-# %%
-# !! {"metadata":{
-# !!   "colab": {
-# !!     "base_uri": "https://localhost:8080/",
-# !!     "height": 299
-# !!   },
-# !!   "id": "EqWz9HxrD7vZ",
-# !!   "executionInfo": {
-# !!     "status": "ok",
-# !!     "timestamp": 1706988866829,
-# !!     "user_tz": 360,
-# !!     "elapsed": 7924,
-# !!     "user": {
-# !!       "displayName": "Uriel Garc\u00eda",
-# !!       "userId": "03386744220426758265"
-# !!     }
-# !!   },
-# !!   "outputId": "bfe22847-f22c-4ef8-fed7-8260d34f76d5"
-# !! }}
+
+#----------------------------------------------------------
 if __name__ == '__main__':
     main()
-
-# %%
-# !! {"main_metadata":{
-# !!   "colab": {
-# !!     "provenance": [],
-# !!     "authorship_tag": "ABX9TyM93/cVKGMRc7nAySuhftID"
-# !!   },
-# !!   "kernelspec": {
-# !!     "name": "python3",
-# !!     "display_name": "Python 3"
-# !!   },
-# !!   "language_info": {
-# !!     "name": "python"
-# !!   }
-# !! }}
