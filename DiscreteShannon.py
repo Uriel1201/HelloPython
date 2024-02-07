b41f9c2358eb4afdc51a058bcd384f6db14783ab
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-02-07 13:58:43 -0600

    Add files via upload

---
+++ DiscreteShannon.py
@@ -0,0 +1,194 @@
+# %%
+# !! {"metadata":{
+# !!   "id": "xfbYDjzmPCuB"
+# !! }}
+"""
+# The Discrete Shannon Measure
+The qualities of this formula "H" play a central role in information theory as measure of information, choice and uncertainty. This measure for example is the H in Boltzmann's famous H Theorem.
+
+This quantity has some interesting properties:
+> 1. When we are certain of the outcomes in a set of observations does H vanish, otherwise H is positive.
+> 2. For a total of m possible results in a set of observations, H is maximum when all proportions of this possibilities are equal. This is also the most uncertain situation.
+> 3. The uncertainty is never increased by experience. It will be decreased unless this experience is irrelevant (independent), in which case uncertainty is not changed.
+"""
+
+# %%
+# !! {"metadata":{
+# !!   "id": "67DnQjXuPDEI",
+# !!   "executionInfo": {
+# !!     "status": "ok",
+# !!     "timestamp": 1707334621592,
+# !!     "user_tz": 360,
+# !!     "elapsed": 1643,
+# !!     "user": {
+# !!       "displayName": "Uriel Garc\u00eda",
+# !!       "userId": "03386744220426758265"
+# !!     }
+# !!   }
+# !! }}
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# %%
+# !! {"metadata":{
+# !!   "id": "JvBAig72Hj2j"
+# !! }}
+"""
+This exercise works according to the following:
+> 1. A script is used to generate multiple sets of data.
+> 2. The data is generated according to a Bernoulli distribution, where the parameter ( p ) is a particular value between 0 and 1.
+> 3. After generating the data, the script roughly determines the uncertainty for each set.
+> 4. Finally, the script generates a plot based on the data and uncertainty information.
+"""
+
+# %%
+# !! {"metadata":{
+# !!   "id": "80Bm92muPXkX",
+# !!   "executionInfo": {
+# !!     "status": "ok",
+# !!     "timestamp": 1707334626356,
+# !!     "user_tz": 360,
+# !!     "elapsed": 280,
+# !!     "user": {
+# !!       "displayName": "Uriel Garc\u00eda",
+# !!       "userId": "03386744220426758265"
+# !!     }
+# !!   }
+# !! }}
+def distBer(p):
+    if p > 1 or p < 0:
+        raise ValueError(f'p must represent a probability measure')
+    ber = np.array([1.0 - p, 1.0])
+    return ber
+
+# %%
+# !! {"metadata":{
+# !!   "id": "gUHdXkfRVtuW",
+# !!   "executionInfo": {
+# !!     "status": "ok",
+# !!     "timestamp": 1707334629345,
+# !!     "user_tz": 360,
+# !!     "elapsed": 292,
+# !!     "user": {
+# !!       "displayName": "Uriel Garc\u00eda",
+# !!       "userId": "03386744220426758265"
+# !!     }
+# !!   }
+# !! }}
+def sampling(dist):
+    u = np.random.rand()
+    n = len(dist)
+    if u <= dist[0]:
+        return 0
+    for i in range(1, n):
+        if u > dist[i - 1] and u <= dist[i]:
+            return i
+    return n
+
+# %%
+# !! {"metadata":{
+# !!   "id": "j-DUVxFwosEN",
+# !!   "executionInfo": {
+# !!     "status": "ok",
+# !!     "timestamp": 1707334633235,
+# !!     "user_tz": 360,
+# !!     "elapsed": 246,
+# !!     "user": {
+# !!       "displayName": "Uriel Garc\u00eda",
+# !!       "userId": "03386744220426758265"
+# !!     }
+# !!   }
+# !! }}
+def discreteShannon(array, m):
+    if m < 0:
+        raise ValueError(f'm must be a cardinal')
+    f = np.zeros(m, dtype = int)
+    n = len(array)
+    for i in range(n):
+        if array[i] < 0:
+            raise ValueError(f'The entries must be no negative')
+        elif array[i] == 0:
+            f[0] += 1
+        elif array[i] < m:
+            f[array[i]] += 1
+    h = 0.0
+    for i in range(m):
+        if f[i] > 0:
+            p = 1.0 * f[i] / n
+            arg = -1.0 * p * np.log(p) / np.log(2)
+            h += arg
+    return h
+
+# %%
+# !! {"metadata":{
+# !!   "id": "KcudteibPEtb",
+# !!   "executionInfo": {
+# !!     "status": "ok",
+# !!     "timestamp": 1707334993628,
+# !!     "user_tz": 360,
+# !!     "elapsed": 270,
+# !!     "user": {
+# !!       "displayName": "Uriel Garc\u00eda",
+# !!       "userId": "03386744220426758265"
+# !!     }
+# !!   }
+# !! }}
+def main():
+    lz = 50
+    x = np.linspace(0, 1, lz, dtype = float)
+    y = []
+    for i in range(len(x)):
+        dist = distBer(x[i])
+        a = []
+        t = 1000
+        for j in range(t):
+            a.append(sampling(dist))
+        a_array = np.array(a)
+        y.append(discreteShannon(a_array, 2))
+    y_array = np.array(y)
+    plt.figure(figsize = (8, 9))
+    sns.set(style = 'darkgrid')
+    plt.title(f'{lz} SAMPLES OF SIZE {t}')
+    plt.ylabel(f'Estimated Uncertainty')
+    plt.xlabel(f'sample with ~ Ber(p)')
+    sns.scatterplot(x = x, y = y_array, color = '#088F8F')
+    plt.tight_layout()
+    plt.show()
+
+# %%
+# !! {"metadata":{
+# !!   "colab": {
+# !!     "base_uri": "https://localhost:8080/",
+# !!     "height": 395
+# !!   },
+# !!   "id": "RGWN-ZNKPUJB",
+# !!   "executionInfo": {
+# !!     "status": "ok",
+# !!     "timestamp": 1707334998225,
+# !!     "user_tz": 360,
+# !!     "elapsed": 918,
+# !!     "user": {
+# !!       "displayName": "Uriel Garc\u00eda",
+# !!       "userId": "03386744220426758265"
+# !!     }
+# !!   },
+# !!   "outputId": "647aa62d-b5ea-4550-d84e-0f9b6443b545"
+# !! }}
+if __name__ == '__main__':
+    main()
+
+# %%
+# !! {"main_metadata":{
+# !!   "colab": {
+# !!     "provenance": [],
+# !!     "authorship_tag": "ABX9TyMCE4hqHnpxHqnSo/oSFy2C"
+# !!   },
+# !!   "kernelspec": {
+# !!     "name": "python3",
+# !!     "display_name": "Python 3"
+# !!   },
+# !!   "language_info": {
+# !!     "name": "python"
+# !!   }
+# !! }}
